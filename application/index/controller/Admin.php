57ac9ccfd50a7dae094cc970465c0f59715b807c
cj <545082957@qq.com>
2018-02-19 14:03:18 +0800

    版本初始

---
+++ application/index/controller/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\index\controller;
+
+/**
+ * Description of admin
+ *
+ * @author 陈
+ */
+class Admin {
+    //put your code here
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\index\controller;
+
+class Index {
+
+    public function index() {
+        return "主页！";
+    }
+    public function hello() {
+        return "hello";
+    }
+}
